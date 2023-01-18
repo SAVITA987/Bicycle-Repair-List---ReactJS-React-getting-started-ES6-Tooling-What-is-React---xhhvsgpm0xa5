5fb65c9c5ed340a65310d73db38fbcc5c7dbc50b
savitatekale <110305120+SAVITA987@users.noreply.github.com>
2023-01-18 18:06:56 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -19,6 +19,23 @@ const reducer = (state = [],action = {}) => {
   
 export default reducer;
 */
+/*
+reducer is defined here and is exproted.
+This reducer should handle all the actions.
+Example of how to use reducer is as follows:
+const reducer = (state = [],action = {}) => {
+  switch(action.type){
+    case 'actionType1': 
+      return changedState1;
+    
+    case 'actionType2':
+      return changedState2;
+    default: 
+      return state;
+    }
+  
+export default reducer;
+*/
 import { v4 as uuidv4 } from "uuid";
 import { combineReducers } from "redux";
 
